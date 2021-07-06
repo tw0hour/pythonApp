aeb32c239fe644087b6a8ef553dd373bf24fe57b
xof <ljehanno@myges.fr>
2021-07-06 19:16:44 +0200

    commentaires

--- initPyGame.py
+++ initPyGame.py
@@ -1,9 +1,11 @@
 import pygame
-
 pygame.init()
+
+# app name
 pygame.display.set_caption("Jeu de plateau")
+
+# window's size
 screen = pygame.display.set_mode((1500, 1000))
+
+# load image
 background = pygame.image.load('ressources/Razer-H1-Wallpaper-2560x1440_290520.png')
-
-running = True
-
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from initPyGame import *
 
+running = True
+
 while running:
+    # arriere plan image
     screen.blit(background, (-500, -200))
     pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
