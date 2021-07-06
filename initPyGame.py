352ab02ca319cacbc280eb75a576b8d24dde1cf9
xof <ljehanno@myges.fr>
2021-07-06 19:13:13 +0200

    la fenetre avec un fond random :')

---
+++ initPyGame.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+pygame.display.set_caption("Jeu de plateau")
+screen = pygame.display.set_mode((1500, 1000))
+background = pygame.image.load('ressources/Razer-H1-Wallpaper-2560x1440_290520.png')
+
+running = True
+
